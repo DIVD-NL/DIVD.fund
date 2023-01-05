54efe2cbab78b8e81c12f07c9220058e8fdc5f03
Frank Breedijk <f.breedijk@divd.nl>
2023-01-05 16:23:40 +0100

    Moving from CircleCI to GitHub Actions

--- test_it.sh
+++ test_it.sh
@@ -3,8 +3,11 @@
 docker pull mrseccubus/github-pages:latest
 docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mrseccubus/github-pages \
 -c "
+	set -e
 	cd /root/project/;
-	rm -rf _site/* jekyll-build.log;
-	jekyll build  --future 2>&1 | tee jekyll-build.log
+	bundle install
+	git config --global --add safe.directory /root/project/csirt.divd.nl
+	rm -rf _site/*;
+	bundle exec jekyll build  --future 2>&1 |grep -Ev 'rb:[0-9]+: warning';
 	./proof_html.sh
 "
